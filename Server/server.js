6d8f3780557fd93bb79a5df197f7582e84abd90f
sujanst9644 <sujan.st98644@gmail.com>
2024-12-25 21:33:45 +0545

    vercel

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://ice-cream-api-red.vercel.app/", 
+    origin: "https://ice-cream-xi-lac.vercel.app/", 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
